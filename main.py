65c78c1a701457e7ba103cebdc0661b986a3278f
sablazmtokill <166620289+sablazmtokill@users.noreply.github.com>
2024-11-24 19:47:07 +0200

    Add files via upload
    /contact_to_managment command was updated, also Arizona Drake server's ip, discord link, forume link been added

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import disnake
 from disnake.ext import commands
-from servers import servers_class1, servers_class2, servers_class3
-from forume_sections import sections_class1, sections_class2, sections_class3
-from servers_ip import api_class1, api_class2, api_class3
+from servers import servers_class1, servers_class2
+from forume_sections import sections_class1, sections_class2
+from servers_ip import api_class1, api_class2
+from admins import admins_class
 from Nicknames import nickname
 from Passwords import password_func
 import disnake.ext
@@ -23,17 +24,14 @@ async def on_ready():
 async def generate_nickname(interaction: disnake.ApplicationCommandInteraction):
 
     nick = nickname()
-    emb = disnake.Embed(title = f"{nick}", color = disnake.Colour.dark_gray()) 
+    emb = disnake.Embed(title = f"{nick}", color = disnake.Colour.dark_gray())
 
     if interaction.guild == None :
         await interaction.response.send_message(embed=emb)
-        
-    else :  
-        await interaction.response.send_message("Проверьте личные сообщения") 
-        await interaction.author.send(embed=emb) 
-        
 
-    
+    else :
+        await interaction.response.send_message("Проверьте личные сообщения")
+        await interaction.author.send(embed=emb)
 
 
 
@@ -45,8 +43,8 @@ async def generate_password(interaction: disnake.ApplicationCommandInteraction):
 
     if interaction.guild == None :
         await interaction.response.send_message(embed=emb)
-    else : 
-        await interaction.response.send_message("Проверьте личные сообщения") 
+    else :
+        await interaction.response.send_message("Проверьте личные сообщения")
         await interaction.author.send(embed=emb)
 
 
@@ -56,20 +54,20 @@ async def discord_servers(interaction: disnake.ApplicationCommandInteraction):
 
     view1 = servers_class1()
     view2 = servers_class2()
-    view3 = servers_class3()
 
     emb = disnake.Embed(title = "Выберите интересующий сервер :", color = disnake.Colour.dark_gray())
-    
+
     if interaction.guild == None :
         await interaction.response.send_message(embed=emb)
         await interaction.author.send(view=view1)
         await interaction.author.send(view=view2)
-        await interaction.author.send(view=view3)
+
     else :
-        await interaction.response.send_message("Проверьте личные сообщения") 
-        await interaction.author.send(embed=emb, view=view1)
+        await interaction.response.send_message("Проверьте личные сообщения")
+        await interaction.author.send(embed=emb)
+        await interaction.author.send(view=view1)
         await interaction.author.send(view=view2)
-        await interaction.author.send(view=view3)
+
 
 
 
@@ -78,7 +76,6 @@ async def forume_sections(interaction: disnake.ApplicationCommandInteraction):
 
     view1 = sections_class1()
     view2 = sections_class2()
-    view3 = sections_class3()
 
     emb = disnake.Embed(title = "Выберите интересующий форум :", color = disnake.Colour.dark_gray())
 
@@ -86,12 +83,13 @@ async def forume_sections(interaction: disnake.ApplicationCommandInteraction):
         await interaction.response.send_message(embed=emb)
         await interaction.author.send(view=view1)
         await interaction.author.send(view=view2)
-        await interaction.author.send(view=view3)
+
     else :
-        await interaction.response.send_message("Проверьте личные сообщения") 
-        await interaction.author.send(embed=emb, view=view1)
+        await interaction.response.send_message("Проверьте личные сообщения")
+        await interaction.author.send(embed=emb)
+        await interaction.author.send(view=view1)
         await interaction.author.send(view=view2)
-        await interaction.author.send(view=view3)
+
 
 
 
@@ -100,7 +98,6 @@ async def servers_ip(interaction: disnake.ApplicationCommandInteraction):
 
     view1 = api_class1()
     view2 = api_class2()
-    view3 = api_class3()
 
     emb = disnake.Embed(title = "Выберите интересующий айпи адрес :", color = disnake.Colour.dark_gray())
 
@@ -108,34 +105,34 @@ async def servers_ip(interaction: disnake.ApplicationCommandInteraction):
         await interaction.response.send_message(embed=emb)
         await interaction.author.send(view=view1)
         await interaction.author.send(view=view2)
-        await interaction.author.send(view=view3)
+
     else :
-        await interaction.response.send_message("Проверьте личные сообщения") 
-        await interaction.author.send(embed=emb, view=view1)
+        await interaction.response.send_message("Проверьте личные сообщения")
+        await interaction.author.send(embed=emb)
+        await interaction.author.send(view=view1)
         await interaction.author.send(view=view2)
-        await interaction.author.send(view=view3)
+
 
 
 
 @bot.slash_command(description="Связаться с руководством")
 async def contact_to_managment(interaction: disnake.ApplicationCommandInteraction):
 
-    managment = "𝘀𝗵𝗼𝘄𝗯𝗶𝘇𝗱𝗲𝗳 - гл. руководство проекта\n 𝘀𝗮𝗯𝗹𝗮𝘇𝗺𝘁𝗼𝗸𝗶𝗹𝗹 - тех. руководство проекта (discord)\n 𝗸𝗿𝗲𝗽𝗸𝗶𝘆𝗼𝗿𝗲𝘅 - q&a проекта\n 𝗺𝟭𝗺𝗼𝘇𝘇𝗮 - медиа-менеджер проекта"
+    view1 = admins_class()
 
-    emb = disnake.Embed(title = "𝗧𝗵𝗲 𝗽𝗿𝗶𝗻𝗰𝗲𝗼𝗳𝘀𝗰𝗮𝗹𝗲 𝘁𝗲𝗮𝗺", description = f"{managment}",color = disnake.Colour.dark_purple())
-   
+    emb = disnake.Embed(title = "Выберите с администрацией какого сервера вам нужно связаться :", color = disnake.Colour.dark_gray())
 
-    if interaction.guild == None :
+    if interaction.guild == None :      # in dm
         await interaction.response.send_message(embed=emb)
-    else :    
-        await interaction.response.send_message("Проверьте личные сообщения") 
+        await interaction.author.send(view=view1)
+    else :      # in global chat
+        await interaction.response.send_message("Проверьте личные сообщения")
         await interaction.author.send(embed=emb)
+        await interaction.author.send(view=view1)
 
 
 
 
-
-        
 
 with open('TOKEN', 'r') as file:
     token = file.read()
